b967793fb6a25c9d7d115c8fc507fc794700e0ad
tttAlFttt <0hp3261131m102u@ezweb.ne.jp>
2023-03-15 13:59:43 +0900

    修正

--- lib/graphScreen.dart
+++ lib/graphScreen.dart
@@ -1,26 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter_app/lineChart.dart';
-import 'package:flutter_app/pricePoints.dart';
 
+class GraphScreen extends StatelessWidget {
+  const GraphScreen({super.key});
 
-
-class GraphPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<ChartData> chartData = [
+      ChartData('David', 25),
+      ChartData('Steve', 38),
+      ChartData('Jack', 34),
+      ChartData('Others', 52)
+    ];
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Flutter Chart"),
-      ),
-      body: content(),
-    );
+        body: Center(
+            child: Container(
+                child: SfCircularChart(series: <CircularSeries>[
+      // Render pie chart
+      PieSeries<ChartData, String>(
+          dataSource: chartData,
+          pointColorMapper: (ChartData data, _) => data.color,
+          xValueMapper: (ChartData data, _) => data.x,
+          yValueMapper: (ChartData data, _) => data.y)
+    ]))));
   }
+}
 
-  Widget content() {
-    return Center(
-      child: Container(
-        child: LineChartWidget(pricePoints),
-      ),
-    );
-  }
+class ChartData {
+  ChartData(this.x, this.y, [this.color]);
+  final String x;
+  final double y;
+  final Color color;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/graphScreen.dart';
-import 'package:flutter_app/dollScreen.dart';
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter_app/lineChart.dart';
-import 'package:flutter_app/pricePoints.dart';
 
 void main() => runApp(const MyApp());
 
@@ -27,13 +24,7 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   int _selectIndex = 0;
 
-  var _pages = <Widget>[
-    ListView(
-      children: [Text('data')],
-    ),
-    DollPage(),
-    GraphPage(),
-  ];
+  var _pages = <Widget>[];
 
   void _onTapItem(int index) {
     setState(() {
@@ -47,69 +38,53 @@ class _MainPageState extends State<MainPage> {
       appBar: AppBar(
         //タイトル
         title: const Text(
-          '猫背 BOUSHI × BOUSHI',
-          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+          '猫背防止帽子',
+          style: TextStyle(
+              fontSize: 23,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(250, 134, 109, 18)),
         ),
-        backgroundColor: Colors.blueGrey, //背景色
+        backgroundColor: Color.fromARGB(255, 244, 214, 116), //背景色
         toolbarHeight: 60,
       ),
-      //メニュー
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blueGrey,
-              ),
-              child: Text(
-                'メニュー',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 30,
-                ),
-              ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/back.png'),
+              fit: BoxFit.fill,
             ),
-            //プロフィールアイコン
-            ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('プロフィール'),
-            ),
-            //設定アイコン
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text('設定'),
-            ),
-          ],
+          ),
         ),
       ),
-      body: _pages[_selectIndex],
+      //_pages[_selectIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.home, //ホームアイコン
-              size: 50, //アイコンサイズ
-            ),
-            label: 'Home', //ラベル
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.airline_seat_recline_normal, //人形アイコン
+              Icons.airline_seat_recline_normal, //ホームアイコン
               size: 50, //アイコンサイズ
             ),
             label: 'Doll', //ラベル
           ),
           BottomNavigationBarItem(
             icon: Icon(
+              Icons.assessment, //人形アイコン
+              size: 50, //アイコンサイズ
+            ),
+            label: 'Graph1', //ラベル
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
               Icons.assessment, //グラフアイコン
               size: 50, //アイコンサイズ
             ),
-            label: 'Graph', //ラベル
+            label: 'Graph2', //ラベル
           ),
         ],
-        selectedItemColor: Colors.amber[800], //選択しているアイコンの色
-        backgroundColor: Colors.blueGrey, //背景色
+        selectedItemColor: Color.fromARGB(255, 255, 85, 0), //選択しているアイコンの色
+        backgroundColor: Color.fromARGB(255, 244, 214, 116), //背景色
         currentIndex: _selectIndex,
         onTap: _onTapItem,
       ),
